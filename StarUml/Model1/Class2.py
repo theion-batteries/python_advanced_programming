fed15ddd95b0223ac726de39f1854253870524ac
sami-theion <sami@theion.de>
2022-03-02 18:32:25 +0100

    updated files

--- StarUml/Model1/Class2.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-#-*- coding: utf-8 -*-
-
-class Class2:
-    def __init__(self):
-        self.Attribute1 = None
-
--- StarUml/Model1/Class3.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-#-*- coding: utf-8 -*-
-
-class Class3:
-    def __init__(self):
-
-    def Operation1(self, ):
-        pass
-
